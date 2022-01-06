547daf4a34f3d10e1903ec966b5847a1d29e50d9
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-01-06 17:41:46 +0530

    Setup initialize DOM

--- src/dom-manager.js
+++ src/dom-manager.js
@@ -1,1 +1,75 @@
+import { publish, subscribe } from "./topic-manager";
+import {
+  INITIALIZE_DOM,
+  ADD_FORM_TO_DISPLAY,
+  REMOVE_FORM_FROM_DISPLAY,
+} from "./topics";
 import "./style.css";
+
+(function formHandler() {
+  function addFormToDOM(topic, data) {
+    const overlay = document.querySelector(".overlay");
+    const formContainer = document.querySelector(data.formSelector);
+    overlay.classList.add("active");
+    formContainer.classList.add("active");
+  }
+  subscribe(ADD_FORM_TO_DISPLAY, addFormToDOM);
+
+  function removeFormFromDOM(topic) {
+    const activeNodeList = document.querySelectorAll(".active");
+    activeNodeList.forEach((activeNode) => {
+      activeNode.classList.remove("active");
+    });
+
+    const formsNodeList = document.querySelectorAll("form");
+    formsNodeList.forEach((form) => {
+      form.reset();
+    });
+  }
+  subscribe(REMOVE_FORM_FROM_DISPLAY, removeFormFromDOM);
+
+  function initialize(topic) {
+    const overlay = document.querySelector(".overlay");
+    overlay.addEventListener("click", (event) => {
+      if (event.target.classList.contains("form-flex-container")) {
+        publish(REMOVE_FORM_FROM_DISPLAY);
+      }
+    });
+  }
+  subscribe(INITIALIZE_DOM, initialize);
+})();
+
+(function projectManager() {
+  function initializeAddProjectButton() {
+    const addProjectButton = document.querySelector(
+      ".projects-section > button"
+    );
+
+    addProjectButton.addEventListener("click", () => {
+      publish(ADD_FORM_TO_DISPLAY, { formSelector: ".project-form-container" });
+    });
+  }
+
+  function initialize(topic) {
+    initializeAddProjectButton();
+  }
+  subscribe(INITIALIZE_DOM, initialize);
+})();
+
+(function todoManager() {
+  function initializeAddTodoButton() {
+    const addTodoButton = document.querySelector(".float-button");
+    addTodoButton.addEventListener("click", () => {
+      publish(ADD_FORM_TO_DISPLAY, { formSelector: ".todo-form-container" });
+    });
+  }
+
+  function initialize(topic) {
+    initializeAddTodoButton();
+  }
+  subscribe(INITIALIZE_DOM, initialize);
+})();
+
+(function tester() {
+  publish(INITIALIZE_DOM);
+})();
---
+++ src/topics.js
@@ -0,0 +1,11 @@
+const INITIALIZE_DOM = "DOM: initialize";
+// Data: None
+
+const ADD_FORM_TO_DISPLAY = "DOM: add form to display";
+// Data: { formSelector }
+// formSelector is the css selector of the form
+
+const REMOVE_FORM_FROM_DISPLAY = "DOM: remove form from display";
+// Data: None
+
+export { INITIALIZE_DOM, ADD_FORM_TO_DISPLAY, REMOVE_FORM_FROM_DISPLAY };
