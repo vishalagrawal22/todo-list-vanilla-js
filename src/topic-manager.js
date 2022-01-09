d49243ccc2e8304d51d738260e0f44fa5b9b0117
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-01-10 01:09:15 +0530

    Remove unused unsubscribe method

--- src/topic-manager.js
+++ src/topic-manager.js
@@ -1,19 +1,11 @@
-import {
-  subscribe as sub,
-  unsubscribe as unsub,
-  publish as pub,
-} from "pubsub-js";
+import { subscribe as sub, publish as pub } from "pubsub-js";
 
 function subscribe(topic, callback) {
   return sub(topic, callback);
-}
-
-function unsubscribe(token) {
-  unsub(token);
 }
 
 function publish(topic, data) {
   return pub(topic, data);
 }
 
-export { subscribe, unsubscribe, publish };
+export { subscribe, publish };
